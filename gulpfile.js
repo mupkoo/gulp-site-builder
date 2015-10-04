cf367a007a75d0bc0697fdfff9e03f20c16e98e9
Mirko Akov <mupkoo@gmail.com>
2015-10-04 16:50:21 +0300

    Add sourcemaps support for SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 /* jslint node: true */
 var gulp         = require('gulp');
 var browserSync  = require('browser-sync').create();
+var sourcemaps   = require('gulp-sourcemaps');
 var sass         = require('gulp-sass');
 
 gulp.task('styles:dev', function () {
     return gulp.src('app/styles/app.scss', { base: 'app' })
+        .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(sourcemaps.write({
+            includeContent: false,
+            sourceRoot: '/'
+        }))
         .pipe(gulp.dest('tmp'))
         .pipe(browserSync.stream());
 });
