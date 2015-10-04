16e15828dc046adcbd05333681f11261b191e519
Mirko Akov <mupkoo@gmail.com>
2015-10-04 15:49:22 +0300

    Add and setup SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,19 @@
 /* jslint node: true */
 var gulp         = require('gulp');
 var browserSync  = require('browser-sync').create();
+var sass         = require('gulp-sass');
 
-gulp.task('serve', [], function () {
+gulp.task('styles:dev', function () {
+    return gulp.src('app/styles/app.scss', { base: 'app' })
+        .pipe(sass())
+        .pipe(gulp.dest('tmp'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('serve', ['styles:dev'], function () {
     browserSync.init({
         port: 8091,
+        open: false,
         ui: {
             port: 8092
         },
