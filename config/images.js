36747b4c365c05ce6ddb89c26774885942bf6ec5
Mirko Akov <mupkoo@gmail.com>
2015-10-04 17:31:55 +0300

    Setup responsive images

---
+++ config/images.js
@@ -0,0 +1,19 @@
+/* jslint node: true */
+
+exports.images = {
+    'images/island.jpg': [{
+        width: 150,
+        rename: 'images/island-150.jpg'
+    }, {
+        width: 150,
+        rename: 'images/island-x1.jpg'
+    }, {
+        width: 300,
+        rename: 'images/island-x2.jpg'
+    }]
+};
+
+exports.config = {
+    quality: 90,
+    errorOnUnusedImage: false
+};
--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ var sourcemaps   = require('gulp-sourcemaps');
 var sass         = require('gulp-sass');
 var browserify   = require('browserify');
 var babelify     = require('babelify');
+var responsive   = require('gulp-responsive');
+var images       = require('./config/images');
 
 gulp.task('javascripts:dev', function () {
     return browserify('app/javascripts/app.js')
@@ -29,13 +31,20 @@ gulp.task('styles:dev', function () {
         .pipe(browserSync.stream());
 });
 
+gulp.task('images:dev', function () {
+    return gulp.src('app/images/**/*.{png,jpg,jpeg,gif,svg}', { base: 'app' })
+        .pipe(responsive(images.images, images.config))
+        .pipe(gulp.dest('tmp'));
+});
+
 gulp.task('watch', function () {
     gulp.watch('app/javascripts/**/*.js', ['javascripts:dev:reload']);
     gulp.watch('app/styles/**/*.scss', ['styles:dev']);
+    gulp.watch('app/images/**/*', ['images:dev']).on('change', browserSync.reload);
     gulp.watch('app/**/*.html').on('change', browserSync.reload);
 });
 
-gulp.task('serve', ['javascripts:dev', 'styles:dev', 'watch'], function () {
+gulp.task('serve', ['javascripts:dev', 'styles:dev', 'images:dev', 'watch'], function () {
     browserSync.init({
         port: 8091,
         open: false,
